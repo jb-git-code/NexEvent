7682db4e499d813403ef4cc8616f91e25d2cc9ec
jb-git-code <jayanshub18@gmail.com>
2026-06-03 23:44:48 +0530

    bottom nav pages

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:nexevent/screens/auth/login_screen.dart';
+import 'package:nexevent/screens/home/events_page.dart';
+import 'package:nexevent/screens/home/my_events_page.dart';
+import 'package:nexevent/screens/home/profile_page.dart';
 import 'package:nexevent/services/auth_service.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +14,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Widget> pages = [EventsPage(), MyEventsPage(), ProfilePage()];
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser;
@@ -36,7 +41,25 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Center(child: Text('Home Page')),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        // showElevation: true,
+        // selectedIndex: _currentIndex,
+        // onItemSelected: (index) {
+        //   setState(() => _currentIndex = index);
+        // },
+        currentIndex: _selectedIndex,
+        onTap: (value) => {
+          setState(() {
+            _selectedIndex = value;
+          }),
+        },
+        items: [
+          BottomNavigationBarItem(label: 'Events', icon: Icon(Icons.event)),
+          BottomNavigationBarItem(label: 'My Events', icon: Icon(Icons.event)),
+          BottomNavigationBarItem(label: 'Profile', icon: Icon(Icons.event)),
+        ],
+      ),
     );
   }
 }
---
+++ lib/screens/home/my_events_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyEventsPage extends StatefulWidget {
+  const MyEventsPage({super.key});
+
+  @override
+  State<MyEventsPage> createState() => _MyEventsPageState();
+}
+
+class _MyEventsPageState extends State<MyEventsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('My Events Page'),
+      ),
+    );
+  }
+}
